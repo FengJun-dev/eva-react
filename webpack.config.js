01fb2158486ef3837c2faec7d082c8f58491ed06
FJ <1298616342@qq.com>
2017-12-05 23:52:18 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const config = {
+    entry:'./main.js',
+    output:{
+        path:'./build',
+        filename:'index.js'
+    },
+    devServer:{
+        inline:true,
+        port:8000
+    },
+    module:{
+        loaders:[{
+            test:/.jsx?$/,
+            exclude:/node_modules/,
+            loader:'babel',
+            query:{
+                presets:['es2015','react']
+            }
+        }]
+    }
+}
+
+module.exports = config;
