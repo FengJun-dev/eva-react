2c899b3b0c1f02e90b7b723b33dbbf62238f393d
FJ <1298616342@qq.com>
2018-02-02 00:47:28 +0800

    commit

---
+++ src/js/components/Detail/detail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const detailstructor = "";
+
+
+class Detail extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
